9710eb04acb573c3289bf2c41e39f5b43aa9751a
philiptang42 <philiptang42@gmail.com>
2015-10-30 22:53:02 -0400

    one solution

--- code.rb
+++ code.rb
@@ -1,3 +1,20 @@
-def caesar_cipher(string ,number)
 
+
+def caesar_cipher(string)
+  shiftyArray = []
+  charLine = string.chars.map(&:ord)
+  alphabet = 26
+
+  shift = 3
+  shift.times do |shift|
+    shiftyArray << charLine.map do |c|
+      ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+    end.join
+  end
+
+  shiftyArray[2]
 end
+
+puts caesar_cipher("a")
+puts caesar_cipher("z")
+puts caesar_cipher("Caesar")
